42adf19b1b485d2f73537ecb171f5e14598d91af
Rei Ibraima <reiibraima1@gmail.com>
2024-07-18 17:03:14 +0200

    Remove default tests and add money tests

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,19 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+describe("test suite : formatCurrency", () => {
+  //gives the suite a name in jasmine
+  it("converts cents into dollars", () => {
+    expect(formatCurrency(2095)).toEqual("20.95");
+  });
+
+  it("Works with 0", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+
+  it("Rounds up to the nearest cent", () => {
+    expect(formatCurrency(2000.5)).toEqual("20.01");
+  });
+
+  it("Rounds down to the nearest cent", () => {
+    expect(formatCurrency(2000.4)).toEqual("20.00");
+  }); //to run a test in jasmine
+});
