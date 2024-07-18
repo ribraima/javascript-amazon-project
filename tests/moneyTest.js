bb515ddc5f0b2f9bbd65ebbc5a6b6d973496f66e
Rei Ibraima <reiibraima1@gmail.com>
2024-07-18 16:14:44 +0200

    Create automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,30 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+console.log("test suite: formatCurrency");
+console.log("Converts cents into dollars");
+if (formatCurrency(2095) === "20.95") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+console.log("Works with 0");
+if (formatCurrency(0) === "0.00") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+console.log("Rounds up to the nearest cent");
+if (formatCurrency(2000.5) === "20.01") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
+
+console.log("Rounds down to the nearest cent");
+if (formatCurrency(2000.4) === "20.00") {
+  console.log("passed");
+} else {
+  console.log("failed");
+}
