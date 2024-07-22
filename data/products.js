e01e3d05b25840c1520b4b32fdaffb7935aa4665
“Rei <reiibraima1@gmail.comgit config --global user.email reiibraima1@gmail.com>
2024-07-22 12:35:47 +0200

    Convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import {formatCurrency} from '../scripts/utils/money.js'
+
 export function getProduct(productId) {
   let matchingProduct;
 
@@ -8,6 +10,31 @@ export function getProduct(productId) {
   });
   return matchingProduct;
 }
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl(){
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice () {
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+}
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -479,4 +506,6 @@ export const products = [
     priceCents: 2400,
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
-];
+].map((productDetails) => {
+  return new Product(productDetails); 
+});
