819667931a77ef2620707451a0236c9f136394cc
“Rei <reiibraima1@gmail.comgit config --global user.email reiibraima1@gmail.com>
2024-07-23 12:56:48 +0200

    Created Appliance Class that also has Instructions and Warranty as extra Info

--- data/products.js
+++ data/products.js
@@ -54,6 +54,27 @@ class Clothing extends Product {
     </a>` // (target = '_blank') tells the link to be opened in a new tab
   }
 }
+
+class Appliance extends Product {
+  instructionsLink;
+  warrantyLink;
+
+  constructor (productDetails){
+    super(productDetails);
+    this.instructionsLink = productDetails.instructionsLink;
+    this.warrantyLink = productDetails.warrantyLink;
+  }
+
+  extraInfoHTML() {
+    return `
+    <a href="${this.instructionsLink}" target="_blank">
+    Instructions
+    </a>
+    <a href="${this.warrantyLink}" target="_blank">
+    Warranty
+    </a>`
+  }
+}
  
 // const date = new Date();
 // console.log(date);
@@ -126,6 +147,9 @@ export const products = [
     },
     priceCents: 1899,
     keywords: ["toaster", "kitchen", "appliances"],
+    type:'appliance',
+    instructionsLink:'images/appliance-instructions.png',
+    warrantyLink:'images/appliance-warranty.png'
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -260,6 +284,9 @@ export const products = [
     },
     priceCents: 3074,
     keywords: ["water boiler", "appliances", "kitchen"],
+    type:'appliance',
+    instructionsLink:'images/appliance-instructions.png',
+    warrantyLink:'images/appliance-warranty.png'
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
@@ -473,6 +500,9 @@ export const products = [
     },
     priceCents: 2250,
     keywords: ["coffeemakers", "kitchen", "appliances"],
+    type:'appliance',
+    instructionsLink:'images/appliance-instructions.png',
+    warrantyLink:'images/appliance-warranty.png'
   },
   {
     id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
@@ -517,6 +547,9 @@ export const products = [
     },
     priceCents: 10747,
     keywords: ["food blenders", "kitchen", "appliances"],
+    type:'appliance',
+    instructionsLink:'images/appliance-instructions.png',
+    warrantyLink:'images/appliance-warranty.png'
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
@@ -554,6 +587,8 @@ export const products = [
 ].map((productDetails) => {
   if (productDetails.type === 'clothing'){
     return new Clothing(productDetails);
+  }else if (productDetails.type === 'appliance'){
+    return new Appliance(productDetails);
   }
   return new Product(productDetails); 
 });
