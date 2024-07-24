d446664ef8fef88df8f365b10de345b705597e5e
“Rei <reiibraima1@gmail.comgit config --global user.email reiibraima1@gmail.com>
2024-07-24 12:24:55 +0200

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -101,6 +101,33 @@ class Appliance extends Product {
 // };
 // console.log(object3.method()); // undefined
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () =>{
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      }else if (productDetails.type === 'appliance'){
+        return new Appliance(productDetails);
+      }
+      return new Product(productDetails); 
+    });
+
+    console.log('load products');
+
+    fun();
+  });
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -592,3 +619,4 @@ export const products = [
   }
   return new Product(productDetails); 
 });
+*/
