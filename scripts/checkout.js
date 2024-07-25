e7ef8d77d7eaf5a0e675d1014d04f663e212ba3d
“Rei <reiibraima1@gmail.comgit config --global user.email reiibraima1@gmail.com>
2024-07-25 14:13:02 +0200

    Convert loadProducts from XHR  to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,16 +1,12 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js'
 //import '../data/backend-practice.js'
 
 Promise.all([
-    new Promise((resolve) => { //similar to done function, lets us control when to go to the next step
-        loadProducts(() => {
-            resolve('value1');
-        });   
-    }),
+    loadProductsFetch(),
     new Promise((resolve) =>{
         loadCart(() => {
             resolve();
--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage, cart } from "../../data/cart.js";
-import { loadProducts } from "../../data/products.js";
+import { loadProducts, loadProductsFetch } from "../../data/products.js";
 
 describe('test suite : renderOrderSummary', () => {
   const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
@@ -8,8 +8,8 @@ describe('test suite : renderOrderSummary', () => {
   
 
   beforeAll((done) => {
-    loadProducts(() =>{
-      done(); //controlls when the functions is done running
+    loadProductsFetch().then(() => {
+      done();
     });
   });
 
