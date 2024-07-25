4b560d526ae0d12d388413b7d05418134de33262
“Rei <reiibraima1@gmail.comgit config --global user.email reiibraima1@gmail.com>
2024-07-25 13:28:29 +0200

    Learn promises and convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,51 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js'
 //import '../data/backend-practice.js'
 
-loadProducts(() =>{
+Promise.all([
+    new Promise((resolve) => { //similar to done function, lets us control when to go to the next step
+        loadProducts(() => {
+            resolve('value1');
+        });   
+    }),
+    new Promise((resolve) =>{
+        loadCart(() => {
+            resolve();
+        });
+    }),
+
+]).then((values)=>{
+    console.log(values);
     renderOrderSummary();
-    renderPaymentSummary();
+    renderPaymentSummary(); 
 });
+
+// new Promise((resolve) => { //similar to done function, lets us control when to go to the next step
+//     loadProducts(() => {
+//         resolve('value1');
+//     });
+
+// }).then((value) =>{
+
+//     console.log(value);
+//     return new Promise((resolve) =>{
+//         loadCart(() => {
+//             resolve();
+//         });
+//     });
+
+// }).then(() =>{
+//     renderOrderSummary();
+//     renderPaymentSummary(); 
+// });
+
+// loadProducts(() =>{
+//     loadCart(() => {
+//         renderOrderSummary();
+//         renderPaymentSummary();
+//     });
+// });
 
